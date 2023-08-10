12c06c33e7ed2c384b1d2e356f125bf2de7ed9ae
Mohammed Sarim <sarimisraiely12@gmail.com>
2023-08-10 22:31:39 +0530

    commit

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const roleTitles = ["Aspiring Data Scientist", "Python Enthusiast", "Full-Stack Web Developer", "Programmer"];
+const roleTitleElement = document.getElementById("role-title");
+
+let index = 0;
+let roleIndex = 0;
+
+function updateRoleTitle() {
+  if (roleIndex >= roleTitles[index].length) {
+    roleIndex = 0;
+    index = (index + 1) % roleTitles.length;
+  }
+  roleTitleElement.textContent = roleTitles[index].substring(0, roleIndex + 1);
+  roleIndex++;
+}
+
+setInterval(updateRoleTitle, 205);
